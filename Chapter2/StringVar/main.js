036a042d792acef18646d183dbb3a551d2dcf1e8
Alan Robb <robbalan61@gmail.com>
2024-07-12 22:04:37 +0100

    Added content

---
+++ Chapter2/StringVar/main.js
@@ -0,0 +1,11 @@
+var username; 
+username = 'Alan';
+var message;
+message = 'See our upcoming range.';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+
+var elNote = document.getElementById('note');
+elNote.textContent = message;
+
---
+++ Chapter2/StringVar/main2.js
@@ -0,0 +1,12 @@
+var title;
+var message;
+
+title = "Alan's Special Offers!";
+message = "25% Off !";
+
+var elTitle = document.getElementById('title');
+var elNote = document.getElementById('note');
+
+elTitle.textContent = title;
+elNote.textContent = message;
+
