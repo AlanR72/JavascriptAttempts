3cb1ab19ec3249461b56a9be5fb4b251d8d9b38e
Alan Robb <robbalan61@gmail.com>
2024-07-15 15:07:03 +0100

    Added content

---
+++ Chapter2/Arrays/main.js
@@ -0,0 +1,13 @@
+var colors;
+colors = ['white', 'black', 'custom'];
+
+var el = document.getElementById("selection");
+el.textContent = colors[2];
+
+/* Alternatively 
+var colors = new Array('white',
+                       'black', 
+                       'custom');
+var el = document.getElementById('selection');
+el.textContent = colors[2];
+*/
