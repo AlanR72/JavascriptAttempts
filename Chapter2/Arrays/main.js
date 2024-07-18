e1cc2d33127460e21224933b76ecadda93f06619
Alan Robb <robbalan61@gmail.com>
2024-07-18 21:33:43 +0100

    Updated content

--- Chapter2/Arrays/main.js
+++ Chapter2/Arrays/main.js
@@ -1,13 +1,16 @@
-var colors;
+/* var colors;
 colors = ['white', 'black', 'custom'];
 
 var el = document.getElementById("selection");
-el.textContent = colors[2];
+el.textContent = colors[0]; */
 
-/* Alternatively 
+/* Alternatively  */
 var colors = new Array('white',
                        'black', 
                        'custom');
+
+/* Colors value can be changed by selecting array position */
+colors[2] = 'beige';                       
+
 var el = document.getElementById('selection');
 el.textContent = colors[2];
-*/
--- Chapter2/StringVar/main2.js
+++ Chapter2/StringVar/main2.js
@@ -3,7 +3,7 @@ var message;
 
 title = "Alan's Special Offers!";
 /* Unable to get hyperlink to work within message! */
-message = "25% Off !";
+message = '<a href=\'sale.html\'> 25% Off !</a>';
 
 var elTitle = document.getElementById('title');
 var elNote = document.getElementById('note');
