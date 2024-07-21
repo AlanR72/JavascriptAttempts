4d5f1923d4f374d73b99ecb2d4c1f186c9fea3f1
Alan Robb <robbalan61@gmail.com>
2024-07-21 17:13:32 +0100

    Updated content

---
+++ Chapter2/Example/main.js
@@ -0,0 +1,28 @@
+var greeting = 'Hi, ';
+var title = 'Alan ';
+var message = 'please check your order :';
+
+var welcome = greeting + title + message;
+var elWelcome = document.getElementById('message');
+elWelcome.textContent = welcome;
+
+var sign = '77 Garnock Street';
+var tiles = sign.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping;
+
+var elSign = document.getElementById('userSign');
+elSign.textContent = sign;
+
+var eltotalTiles = document.getElementById('tiles');
+eltotalTiles.textContent = tiles;
+
+var elsubTotal = document.getElementById('subtotal');
+elsubTotal.textContent = '$' + subTotal;
+
+var elshipping = document.getElementById('shipping');
+elshipping.textContent = '$' + shipping;
+
+var total = document.getElementById('grandTotal')
+total.textContent = '$' + grandTotal;
