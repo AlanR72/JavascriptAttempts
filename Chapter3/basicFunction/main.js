e7707590e854b4e5ca89494f5c3878a61e6167a2
Alan Robb <robbalan61@gmail.com>
2024-07-25 21:34:17 +0100

    Updated files

---
+++ Chapter3/basicFunction/main.js
@@ -0,0 +1,8 @@
+var msg = 'Sign up to receive our newsletter and 20% discount!';
+
+function updateMessage() {
+    var el = document.getElementById('box');
+    el.textContent = msg;
+}
+
+updateMessage();
