8b3fb6b283c7f8f4b9d365d992b0da59ce885517
Alan Robb <robbalan61@gmail.com>
2024-07-28 21:42:21 +0100

    Updated files

---
+++ Chapter3/AnonymousFunctions/main.js
@@ -0,0 +1,17 @@
+//Function Declaration
+//Also Named function
+//Function declarations can be called before declared
+function area(width, height) {
+    return width * height;
+}
+
+var size = area(3,5);
+
+//Function Expression
+//Expressions not processed before interpreter reaches it
+//Cannot be called before discovered
+area = function(width, height) {
+    return width * height;
+};
+
+var size = area(3,5);
