6425c20b39a1e3c7b03726e08dff07fd924bfd5f
Alan Robb <robbalan61@gmail.com>
2024-07-28 21:59:29 +0100

    Updated files

---
+++ Chapter3/IIFE/main.js
@@ -0,0 +1,17 @@
+var area = {function() {
+    var width = 3;
+    var height = 5;
+    return width * height;
+}()};
+
+/* IIFE - Immediately Invoked Function Expression */
+/* IIFE's are not given a name, instead are 
+executed once as the interpreter comes across them.
+The grouping operatore (first set of curly brackets)
+are there to ensure interpreter treats it as an 
+expression.
+The final set of normal parentheses are there to call the 
+function immediately. */
+
+/*Anonymous functions and IIFE's are often used for
+code that only has to run once amongst other uses. */
