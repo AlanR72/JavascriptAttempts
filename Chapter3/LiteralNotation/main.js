db08ce5539f00e37debee9046ae47b0e84100427
Alan Robb <robbalan61@gmail.com>
2024-08-27 23:25:35 +0100

    Updated files

--- Chapter3/LiteralNotation/main.js
+++ Chapter3/LiteralNotation/main.js
@@ -13,3 +13,8 @@ elName.innerHTML = hotel.name;
 
 var elRooms = document.getElementById('roomsLeft');
 elRooms.innerHTML = hotel.checkAvailability();
+
+/*you can also access properties or methods using
+square bracket syntax i.e
+var elName = hotel['name'];
+var elRooms = hotel['checkAvailability'](); */
