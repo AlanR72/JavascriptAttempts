68c448f8b0b16f153052a7e84e7ce45c54335225
Alan Robb <robbalan61@gmail.com>
2024-08-27 22:28:48 +0100

    Updated files

--- Chapter3/FuncMethObjExample/main.js
+++ Chapter3/FuncMethObjExample/main.js
@@ -1,28 +1,26 @@
 /* Script is placed inside an immediately invoked function expression
 which helps protect the scope of the variables*/
 (function() {
-    //Part One:Create hotels object and write out offer details
-
-    //Hotel object using object literal syntax
-    var hotel = {
+     var hotel = {
         name: 'Park',
-        roomRate: 240, //amount in pounds
-        discount: 15, //percentage discount
+        roomRate: 240,
+        discount: 15,
         offerPrice: function() {
-            var offerRate = this.roomRate * ((100 - this.discount) / 100);
+        var offerRate = this.roomRate * ((100 - this.discount) / 100);
             return offerRate; 
         }
     };
-    //Write out the hotel name, standard rate and special rate
+
     var hotelName;
     var roomRate;
-    var specialRate;//Declare variables
+    var specialRate;
 
-    hotelName = document.getElementById('hotelName');
+    hotelName = document.getElementById("hotelName");
     roomRate = document.getElementById('roomRate');
     specialRate = document.getElementById('specialRate');
 
-    hotelName.textContent = '<p>Hello</p>';
+    hotelName.textContent = hotel.name;
     roomRate.textContent = '£' + hotel.roomRate.toFixed(2);
     specialRate.textContent = '£' + hotel.offerPrice();
-})
+    
+}());
